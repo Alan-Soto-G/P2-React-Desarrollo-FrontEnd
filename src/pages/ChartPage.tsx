982385568f7581d62e27382eaef4df19fb57ff3b
laura-cardonnag <laucagomez1615@gmail.com>
2025-05-19 15:18:34 -0500

    Graficos

--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MyCharts from '../components/MyCharts';
+import ChartsL from '../components/ChartsL';
 
 const ChartsPage = () => {
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>Gráficos con ApexCharts</h2>
       <MyCharts />
+      <ChartsL />
     </div>
   );
 };
